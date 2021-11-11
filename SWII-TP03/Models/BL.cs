5fdad2cf27519dd0c47ae15e57360c62bbacd4b1
pimenteltiago <57777242+pimenteltiago@users.noreply.github.com>
2021-11-11 17:00:37 -0300

    Add files via upload

---
+++ SWII-TP03/Models/BL.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SWII_TP03.Models {
+    public class BL {
+        public int id { get; set; }
+        public int numero { get; set; }
+        public String consignee { get; set; }
+        public String navio { get; set; }
+
+    }
+}
