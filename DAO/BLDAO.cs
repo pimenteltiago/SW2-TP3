af2e1693c71e75d164ee3f27b0e679832d0be360
pimenteltiago <57777242+pimenteltiago@users.noreply.github.com>
2021-11-11 17:01:03 -0300

    Add files via upload

---
+++ DAO/BLDAO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SWII_TP03.DAO {
+    public class BLDAO {
+        public void Adiciona(Models.BL bl) {
+            using (var context = new Context()) {
+                context.BL.Add(bl);
+                context.SaveChanges();
+            }
+        }
+
+        public IList<Models.BL> Lista() {
+            using (var contexto = new Context()) {
+                return contexto.BL.ToList();
+            }
+        }
+
+        public void Atualiza(Models.BL bl) {
+            using (var contexto = new Context()) {
+                contexto.Entry(bl).State = System.Data.Entity.EntityState.Modified;
+                contexto.SaveChanges();
+            }
+        }
+
+        public void Deletar(Models.BL bl) {
+            using (var contexto = new Context()) {
+                contexto.BL.Remove(bl);
+                contexto.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Models/Container.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SWII_TP03.Models {
+    public class Container {
+        public int id {get;set;}
+        public int numero { get; set; }
+        public String tipo { get; set; }
+        public int tamanho { get; set; }
+        public int id_bl { get;set; }
+    }
+}
